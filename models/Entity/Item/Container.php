ea125cd1c20035242fa859a2b40b75dfb76ae161
Bill Parrott <bill@chimericdream.com>
2013-09-21 16:46:29 -0500

    adding state to containers and mobs

--- models/Entity/Item/Container.php
+++ models/Entity/Item/Container.php
@@ -1,3 +1,5 @@
 <?php
 class Entity_Item_Container extends Entity_Item {
+    const STATE_OPEN   = 1;
+    const STATE_CLOSED = 0;
 }
--- models/Entity/Mob.php
+++ models/Entity/Mob.php
@@ -3,4 +3,6 @@ require_once dirname(__FILE__) . '/Mob/Peasant.php';
 require_once dirname(__FILE__) . '/Mob/Warrior.php';
 
 class Entity_Mob extends Entity {
+    const STATE_ALIVE = 1;
+    const STATE_DEAD  = 0;
 }
